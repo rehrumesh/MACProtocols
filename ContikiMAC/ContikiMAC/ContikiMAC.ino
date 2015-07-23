a17d50db352e4542f03f118333ff68b943118ac4
Rumesh Eranga <rehrumesh@hotmail.com>
2015-07-23 19:21:04 +0530

    Modifications to radio on and off in ContikiMAC

--- ContikiMAC/ContikiMAC/ContikiMAC.ino
+++ ContikiMAC/ContikiMAC/ContikiMAC.ino
@@ -1,8 +1,30 @@
 //ContikiMCAC arduino implementation for ATtiny85 and NRF24L01
+
+// #define SHORTESET_PACKET_SIZE 43
+
+
+
+//CCA_COUNT_MAX is the number of CCAs to be done for each periodic channel check. Default is 2
+
+#ifdef CONTIKIMAC_CONF_CCA_COUNT_MAX
+#define CCA_COUNT_MAX (CONTIKIMAC_CONF_CCA_COUNT_MAX)
+#else
+#define CCA_COUNT_MAX 2
+#endif
+
 void setup(){
 
 }
 
 void loop(){
 
+}
+
+
+void on(){
+  // turn on the radio
+}
+
+void off(){
+  //turn off the radio
 }
